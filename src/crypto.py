ffd84d31ac3b689bcdc31cd8e6af560e212bbd7a
jteles277 <59253209+jteles277@users.noreply.github.com>
2023-01-05 15:56:08 +0000

    Adds Symetric encryption to Cipher class

--- src/crypto.py
+++ src/crypto.py
@@ -1,3 +1,48 @@
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding  
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
 class Crypto:
     """Cryptographic utilities"""
-    pass
+
+    @classmethod
+    def sym_gen(cls):
+        """Generates a new AESGCM key"""
+        return (AESGCM.generate_key(bit_length=128), os.urandom(12))
+
+    @classmethod
+    def sym_encrypt(cls, data, key, nonce):
+        """Encrypts data given with given AESGCM key"""
+
+        cypher = AESGCM(key) 
+        ct = cypher.encrypt(nonce, data, None)
+
+        #print("\n[Encypted] msg: " + str(data) + ", to: "+ str(ct)) 
+        return ct 
+
+    @classmethod
+    def sym_decrypt(cls, crypted_data, key, nonce):
+        """Decrypts encrypted data given with given AESGCM key"""
+        
+        cypher = AESGCM(key) 
+        data = cypher.decrypt(nonce, crypted_data, None)
+
+        #print("\n[Decrypted] ct: " + str(crypted_data) + ", to: "+ str(data)) 
+        return data 
+
+
+
+
+
+
+""" HOW TO USE: 
+
+msg_ = b'uwu'  
+
+(key,nonce) = Crypto.sym_gen()
+msg = Crypto.sym_decrypt(Crypto.sym_encrypt(msg_, key, nonce), key, nonce)
+
+print("\n")
+    
+"""

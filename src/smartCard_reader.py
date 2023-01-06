4f0bb840dad77393bfd4d1da1d88a91d8bbcc5f3
rafael <rafael.lixa@ua.pt>
2023-01-06 21:54:56 +0000

    more stuff

--- src/smartCard_reader.py
+++ src/smartCard_reader.py
@@ -1,5 +1,7 @@
 import PyKCS11
 import binascii
+from asn1crypto import pem, x509
+
 
 lib = '/usr/lib/x86_64-linux-gnu/pkcs11/opensc-pkcs11.so'
 
@@ -16,6 +18,10 @@ class SmartCardSession():
         self.session = self.pkcs11.openSession(slot, PyKCS11.CKF_SERIAL_SESSION | PyKCS11.CKF_RW_SESSION)
         self.session.login(loginCred)
 
+        self.pubKey =self.session.findObjects([(PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY)])[0]
+        
+        self.prikey =  self.session.findObjects([(PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY)])[0]
+
     def getKeys(self):
         """Retuns Public and Private keys"""
 
@@ -28,6 +34,15 @@ class SmartCardSession():
                   (PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY), 
                   ])[0])
 
+
+    def getPublicPEM(self):
+
+        dict = self.pubKey.to_dict()
+        cka_value = dict.get("CKA_VALUE")
+        cert_der = bytes(cka_value)
+
+
+        return pem.armor('PUBLIC KEY', cert_der)
 
         
 

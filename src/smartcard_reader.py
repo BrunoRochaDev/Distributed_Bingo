26657711eb1c1bd748c6b10ea547251caff061d4
Bruno Rocha Moura <brunorochamoura@proton.me>
2023-02-01 17:01:26 +0000

    Adds type hinting to smartcard script

--- src/smartcard_reader.py
+++ src/smartcard_reader.py
@@ -10,14 +10,12 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from binascii import a2b_hex, b2a_hex
 from cryptography.hazmat.primitives.asymmetric import padding   
 
-
-
 lib = '/usr/lib/x86_64-linux-gnu/pkcs11/opensc-pkcs11.so'
 
 class SmartCardSession():
     """Smart Card Session Utilities"""
 
-    def __init__(self,loginCred: str):
+    def __init__(self,loginCred: str) -> None:
         """Generates a PyKCS11 session, required Citizen Card PIN code"""
         self.pkcs11 = PyKCS11.PyKCS11Lib()
         self.pkcs11.load(lib)
@@ -27,22 +25,18 @@ class SmartCardSession():
         self.session = self.pkcs11.openSession(slot, PyKCS11.CKF_SERIAL_SESSION | PyKCS11.CKF_RW_SESSION)
         self.session.login(loginCred)
 
-        self.pubKey =self.session.findObjects([(PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY)])[0]
+        self.pubKey = self.session.findObjects([(PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY)])[0]
 
-
-
-    def getPublicKey(self):
+    def getPublicKey(self) -> tuple[bytes, bytes]:
         """Returns the modulus and pubExponent corresponding to the smart card's public key as bytes"""
-
         modulus, pubexp = self.session.getAttributeValue(
                 self.pubKey, [PyKCS11.CKA_MODULUS, PyKCS11.CKA_PUBLIC_EXPONENT]
         )
 
-
         return bytes(modulus),bytes(pubexp)
 
-    def sign(self,message: bytes):
-       
+    def sign(self,message: bytes) -> bytes:
+        """Signs a message and returns the signature"""
 
         signature = self.session.sign(
                 self.session.findObjects([(PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY)])[0], message, PyKCS11.Mechanism(PyKCS11.CKM_SHA256_RSA_PKCS, None)
@@ -50,17 +44,13 @@ class SmartCardSession():
 
         return signature
 
-    def close(self):
+    def close(self) -> None:
+        """Closes the PyKCS11 session"""
         self.session.logout()
         self.session.closeSession()
-
-        return
-
-
 
 if __name__ == '__main__':
     
     smart = SmartCardSession("1111") 
     result = smart.getPublicPEM()
     print(result.decode("ascii"))
-
